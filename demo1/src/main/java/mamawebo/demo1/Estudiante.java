2ccb9c832335296fac322965b0d98ebd42b69587
Csar-46 <septiembrecesar@hotmail.com>
2025-05-07 20:34:29 +0200

    JavaFX practica 2

---
+++ demo1/src/main/java/mamawebo/demo1/Estudiante.java
@@ -0,0 +1,25 @@
+package mamawebo.demo1;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+
+@Getter
+@ToString
+public class Estudiante {
+
+    private int nia;
+    private String nombre;
+    private LocalDate fecha_nacimiento;
+
+    public Estudiante(int nia, String nombre, LocalDate fecha_nacimiento){
+
+        this.nia = nia;
+        this.nombre = nombre;
+        this.fecha_nacimiento = fecha_nacimiento;
+
+    }
+
+}
---
+++ demo1/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module mamawebo.demo1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+    requires static lombok;
+    requires com.google.errorprone.annotations;
+
+    opens mamawebo.demo1 to javafx.fxml;
+    exports mamawebo.demo1;
+}
